71b73e4d5d638a920cdc448678100a32db60c243
Nicolás Giudice <n.giudice@shipnow.com.ar>
2026-04-24 16:24:38 -0300

    update dependencies

--- storybook-template/.storybook/preview.jsx
+++ storybook-template/.storybook/preview.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
-import { ThemeProvider } from "../src/components/ThemeProvider/ThemeProvider";
-import { Box } from "../src/components/Box/Box";
-
 /** @type { import('@storybook/react-vite').Preview } */
 const preview = {
-  decorators: [
-    (Story) => (
-      <ThemeProvider>
-        <Box padding="b-32 t-24 x-24">
-          <Story />
-        </Box>
-      </ThemeProvider>
-    ),
-  ],
-
   parameters: {
     backgrounds: {},
     controls: {
